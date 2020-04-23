c33dc2008f902044a091d19d786349a431e7e2ea
AnTy Ali <51858432+Adrian637@users.noreply.github.com>
2020-04-23 21:47:23 +0700

    Add files via upload

---
+++ get.sh
@@ -0,0 +1,23 @@
+apt-get upgrade && apt-get update
+pkg install git
+pkg install wget
+apt-get install netcat
+apt-get install tmux
+apt-get install w3m
+apt-get install python python2 
+apt-get install hydra
+apt-get dist-upgrade
+pkg install clang
+apt-get install nano
+apt-get install vim
+apt-get install coreutils
+apt-get install tar
+apt-get install less
+apt-get install openssh
+pkg install mc
+pkg install figlet
+pkg install php 
+pip install mechanize
+pkg install ncurses-utils
+pkg install sl
+pkg install perl
