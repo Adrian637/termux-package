6a597ceb4045688225c611d86846f85e20362c34
X FO110W <51858432+xfo110w@users.noreply.github.com>
2020-11-27 12:53:16 +0700

    Add files via upload

---
+++ get.sh
@@ -0,0 +1,27 @@
+pkg i root-repo
+apt-get upgrade && apt-get update
+pkg i git -y
+pkg i wget -y 
+apt-get install netcat -y
+apt-get install tmux -y
+apt-get install w3m -y
+apt-get install python python2 -y
+apt-get install hydra -y
+apt-get dist-upgrade 
+pkg i clang -y
+apt-get i nano 
+apt-get install vim -y
+apt-get install coreutils 
+apt-get install tar 
+apt-get install less 
+apt-get install openssh -y
+pkg i mc -y
+pkg i figlet
+pkg i php 
+pip install --upgrade pip
+pip install mechanize
+pkg i ncurses-utils
+pkg i perl
+pkg i sl
+apt-get install proot -y
+pkg i termux-api
